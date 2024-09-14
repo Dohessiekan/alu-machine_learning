8d27e601c1f477515ccc8730bebc097656882381
Dohessiekan <d.gondoyi@alustudent.com>
2024-09-14 03:26:38 -0700

    yes

--- supervised_learning/classification/27-deep_neural_network.py
+++ supervised_learning/classification/27-deep_neural_network.py
@@ -3,9 +3,7 @@
 Defines the DeepNeuralNetwork class that implements
 a deep neural network for multiclass classification.
 """
-
 import numpy as np
-
 
 class DeepNeuralNetwork:
     """
@@ -30,11 +28,9 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) < 1 or not all(isinstance(layer, int) and layer > 0 for layer in layers):
             raise TypeError("layers must be a list of positive integers")
-
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
-
         previous = nx
         for i, layer in enumerate(layers):
             self.__weights["W{}".format(i + 1)] = np.random.randn(layer, previous) * np.sqrt(2 / previous)
@@ -52,7 +48,6 @@ class DeepNeuralNetwork:
     @property
     def weights(self):
         return self.__weights
-
     def forward_prop(self, X):
         """
         Performs forward propagation for the neural network.
@@ -71,7 +66,6 @@ class DeepNeuralNetwork:
             A = 1 / (1 + np.exp(-Z))  # Sigmoid activation
             self.__cache["A{}".format(i + 1)] = A
         return A, self.__cache
-
     def cost(self, Y, A):
         """
         Computes the cost using logistic regression.
@@ -86,7 +80,6 @@ class DeepNeuralNetwork:
         m = Y.shape[1]
         cost = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
         return cost
-
     def evaluate(self, X, Y):
         """
         Evaluates the neural network's predictions.
@@ -102,7 +95,6 @@ class DeepNeuralNetwork:
         cost = self.cost(Y, A)
         prediction = np.where(A >= 0.5, 1, 0)
         return prediction, cost
-
     def gradient_descent(self, Y, alpha=0.05):
         """
         Performs one pass of gradient descent on the neural network.
@@ -114,22 +106,17 @@ class DeepNeuralNetwork:
         m = Y.shape[1]
         L = self.L
         dz = self.__cache["A{}".format(L)] - Y
-
         for i in reversed(range(1, L + 1)):
             A_prev = self.__cache["A{}".format(i - 1)]
             W = self.__weights["W{}".format(i)]
             b = self.__weights["b{}".format(i)]
-
             dW = (1 / m) * np.matmul(dz, A_prev.T)
             db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-
             if i > 1:
                 A_prev = self.__cache["A{}".format(i - 1)]
                 dz = np.matmul(W.T, dz) * A_prev * (1 - A_prev)
-
             self.__weights["W{}".format(i)] -= alpha * dW
             self.__weights["b{}".format(i)] -= alpha * db
-
     def train(self, X, Y, iterations=5000, alpha=0.05, verbose=True, graph=True, step=100):
         """
         Trains the neural network.
@@ -150,17 +137,14 @@ class DeepNeuralNetwork:
             raise ValueError("iterations must be a positive integer")
         if type(alpha) is not float or alpha <= 0:
             raise ValueError("alpha must be positive")
-
         costs = []
         for i in range(iterations):
             A, _ = self.forward_prop(X)
             self.gradient_descent(Y, alpha)
-
             if verbose and i % step == 0:
                 cost = self.cost(Y, A)
                 print(f"Cost after {i} iterations: {cost}")
                 costs.append(cost)
-
         if graph:
             import matplotlib.pyplot as plt
             plt.plot(np.arange(0, iterations, step), costs)
@@ -168,6 +152,5 @@ class DeepNeuralNetwork:
             plt.ylabel("Cost")
             plt.title("Training Cost")
             plt.show()
-
         return self.evaluate(X, Y)
 
