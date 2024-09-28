ec282ee683d56e28ee12640f18e7394a4aa9167c
Dohessiekan <d.gondoyi@alustudent.com>
2024-09-28 00:38:42 -0700

    yes

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python3
-"""
-Defines function that normalizes an unactivated output of a neural network
-using batch normalization
-"""
+'''
+    Function def batch_norm(Z, gamma, beta, epsilon):
+    that normalizes an unactivated output of a neural
+    network using batch normalization
+'''
+
+
+import numpy as np
 
 
 def batch_norm(Z, gamma, beta, epsilon):
-    """
-    Normalizes an unactivated output of a neural network
+    '''
+        Normalizes an unactivated output of a neural network
         using batch normalization
 
-    parameters:
-        Z [numpy.ndarray of shape (m, n)]:
-            unactivated output to normalize
-            m: number of data points
-            n: number of features in Z
-        gamma [numpy.ndarray of shape (1, n)]:
-            contains the scales used for batch normalization
-        beta [numpy.ndarray of shape (1, n)]:
-            contains the offsets used for batch normalization
-        epsilon [float]: small number to avoid division by zero
+        Args:
+            - Z is a numpy.ndarray of shape (m, n) that should be normalized
+                - m is the number of data points
+                - n is the number of features in Z
+            - gamma is a numpy.ndarray of shape (1, n) containing the scales
+                used for batch normalization
+            - beta is a numpy.ndarray of shape (1, n) containing the offsets
+                used for batch normalization
+            - epsilon is a small number used to avoid division by zero
 
-    returns:
-        the normalized Z matrix
-    """
+        Returns:
+            - The normalized Z matrix
+    '''
+    mean = np.mean(Z, axis=0)
+    variance = np.var(Z, axis=0)
+    Z_norm = (Z - mean) / np.sqrt(variance + epsilon)
+    Z_tilde = gamma * Z_norm + beta
+    return Z_tilde
