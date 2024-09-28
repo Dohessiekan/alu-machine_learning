cc05532360f7ad3b60f668afe35de1628f36b6b7
Dohessiekan <d.gondoyi@alustudent.com>
2024-09-28 00:14:44 -0700

    yes

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """
-Defines function that updates the weights with Dropout regularization
-using gradient descent
+    function def dropout_gradient_descent(
+        Y, weights, cache, alpha, keep_prob, L)
+        that updates the weights of a neural network
+        with Dropout regularization using gradient descent:
 """
 
 import numpy as np
@@ -9,51 +11,48 @@ import numpy as np
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
-    Updates the weights with Dropout regularization using gradient descent
+    Updates the weights of a neural network with Dropout regularization
 
-    parameters:
-        Y [one-hot numpy.ndarray of shape (classes, m)]:
-            contains the correct labels for the data
-            classes: number of classes
-            m: number of data points
-        weights [dict]:
-            contains the weights and biases of the network
-        cache [dict]:
-            contains the outputs and dropout masks of each layer
-        alpha [float]:
-            learning rate
-        keep_prob [float]:
-            the probability that a node will be kept
-        L [int]:
-            number of layers in the network
+    Args:
+        - Y is a one-hot numpy.ndarray of shape (classes, m)
+        that contains the correct
+        labels for the data
+        classes is the number of classes
+        - m is the number of data points
+        - weights is a dictionary of the weights and biases
+        of the neural network
+        - cache is a dictionary of the outputs and dropout
+        masks of each layer of
+        the neural network
+        - alpha is the learning rate
+        - keep_prob is the probability that a node will be kept
+        - L is the number of layers of the network
 
-    all layers should use the tanh activation function except last
-    last layer should use softmax activation function
+        - All layers use thetanh activation function except the last,
+        which uses
+        the softmax activation function
 
-    the weights of the network should be updated in place
+    Returns:
+        - The updated weights of the neural Network
     """
     m = Y.shape[1]
-    back = {}
-    for index in range(L, 0, -1):
-        A = cache["A{}".format(index - 1)]
-        if index == L:
-            back["dz{}".format(index)] = (cache["A{}".format(index)] - Y)
-            dz = back["dz{}".format(index)]
+    dz = cache['A' + str(L)] - Y
 
-        else:
-            dz_prev = back["dz{}".format(index + 1)]
-            A_current = cache["A{}".format(index)]
-            back["dz{}".format(index)] = (
-                np.matmul(W_prev.transpose(), dz_prev) *
-                (A_current * (1 - A_current)))
-            dz = back["dz{}".format(index)]
-            dz *= cache["D{}".format(index)]
-            dz /= keep_prob
+    for layer in range(L, 0, -1):
+        A_prev = cache['A' + str(layer - 1)]
+        W = weights['W' + str(layer)]
+        b = weights['b' + str(layer)]
 
-        dW = (1 / m) * (np.matmul(dz, A.transpose()))
+        dW = (1 / m) * np.matmul(dz, A_prev.T)
         db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-        W_prev = weights["W{}".format(index)]
-        weights["W{}".format(index)] = (
-            weights["W{}".format(index)] - (alpha * dW))
-        weights["b{}".format(index)] = (
-            weights["b{}".format(index)] - (alpha * db))
+
+        if layer > 1:
+            D_prev = cache['D' + str(layer - 1)]
+            A_prev = cache['A' + str(layer - 1)]
+            dz = np.matmul(W.T, dz) * (1 - A_prev ** 2)
+            dz *= D_prev  # Apply dropout mask
+            dz /= keep_prob  # Scale the activation for the dropped units
+
+        # Update weights and biases
+        weights['W' + str(layer)] = W - alpha * dW
+        weights['b' + str(layer)] = b - alpha * db
