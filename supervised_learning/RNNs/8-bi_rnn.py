a6cef04cda22e0c77d2c896f1a7e912f5f2b6eaf
Dohessiekan <d.gondoyi@alustudent.com>
2025-01-19 17:07:07 +0200

    updated

--- supervised_learning/RNNs/8-bi_rnn.py
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -1,55 +1,24 @@
-#!/usr/bin/env python3
-"""
-Defines function that performs forward propagation for bidirectional RNN
-"""
-
 import numpy as np
 
+class BidirectionalCell:
+    def __init__(self, i, h, o):
+        self.Whf = np.random.randn(i + h, h)  # Weights for forward direction
+        self.Whb = np.random.randn(i + h, h)  # Weights for backward direction
+        self.Wy = np.random.randn(2 * h, o)   # Weights for output
+        self.bhf = np.zeros((1, h))           # Bias for forward direction
+        self.bhb = np.zeros((1, h))           # Bias for backward direction
+        self.by = np.zeros((1, o))            # Bias for output
 
-def bi_rnn(bi_cell, X, h_0, h_t):
-    """
-    Performs forward propagation for a bidirectional RNN
+    def forward(self, h_prev, x_t):
+        concat = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.dot(concat, self.Whf) + self.bhf)
+        return h_next
 
-    Parameters:
-    - bi_cell: An instance of BidirectionalCell
-    - X: numpy.ndarray of shape (t, m, i), data to be used
-        - t: maximum number of time steps
-        - m: batch size
-        - i: dimensionality of data
-    - h_0: numpy.ndarray of shape (m, h),
-        initial hidden state (forward direction)
-    - h_t: numpy.ndarray of shape (m, h),
-        initial hidden state (backward direction)
+    def backward(self, h_next, x_t):
+        concat = np.concatenate((h_next, x_t), axis=1)
+        h_prev = np.tanh(np.dot(concat, self.Whb) + self.bhb)
+        return h_prev
 
-    Returns:
-    - H: numpy.ndarray of shape (t, m, 2*h), concatenated hidden states
-    - Y: numpy.ndarray of shape (t, m, o), outputs
-    """
-    t, m, _ = X.shape
-    h = h_0.shape[1]
-
-    # Initialize hidden states
-    h_forward = np.zeros((t, m, h))
-    h_backward = np.zeros((t, m, h))
-
-    # Forward propagation (left-to-right)
-    h_prev = h_0
-    for step in range(t):
-        h_prev = bi_cell.forward(h_prev, X[step])
-        h_forward[step] = h_prev
-
-    # Backward propagation (right-to-left)
-    h_prev = h_t
-    for step in range(t - 1, -1, -1):
-        h_prev = bi_cell.backward(h_prev, X[step])
-        h_backward[step] = h_prev
-
-    # Concatenate hidden states
-    H = np.concatenate((h_forward, h_backward), axis=2)
-
-    # Compute outputs for each concatenated hidden state
-    Y = np.zeros((t, m, bi_cell.Wy.shape[1]))
-    for step in range(t):
-        Y[step] = bi_cell.output(H[step])
-
-    return H, Y
+    def output(self, H):
+        y = np.dot(H, self.Wy) + self.by
+        return np.exp(y) / np.sum(np.exp(y), axis=1, keepdims=True)
